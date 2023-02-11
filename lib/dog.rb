9e93dc12c85523656227343c477609f5da8845c7
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-02-11 12:50:12 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+    def initialize
+        @breed = nil
+    end
+    def breed
+        @breed
+    end
+    def breed=(new_breed)
+        @breed = new_breed
+    end
 
 end
